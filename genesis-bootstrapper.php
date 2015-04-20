8c0b6371b2759394ebba19299ecea541ce6471c1
Nate Finch <finchproservices@gmail.com>
2015-04-21 07:42:20 +0900

    added bootstrap links

--- genesis-bootstrapper.php
+++ genesis-bootstrapper.php
@@ -8,3 +8,11 @@ Version: 1.0
 Author URI: http://www.finchproservices.com
 */
 
+function n8f_get_bootstrap() {
+ 
+    wp_enqueue_style('n8f-styles', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css', __FILE__ ) );
+    wp_enqueue_script( 'n8f-scripts', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array( 'jquery' ), '1.0' );
+    
+}
+
+add_action( 'wp_enqueue_scripts', 'n8f_get_bootstrap' );
