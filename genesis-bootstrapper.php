006fefd6c912fefe17fe175f68eed397f8b57dcd
Nate Finch <finchproservices@gmail.com>
2015-04-21 07:51:43 +0900

    bootstrap styles and script seem to enque just fine at genesis_meta

--- genesis-bootstrapper.php
+++ genesis-bootstrapper.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Genesis Bootstrapper
 Plugin URI: http://www.finchproservices.com
-Description: This is a simple plugin that will enque Bootstrap to your Genesis WordPress site.
+Description: This is a simple plugin that will enque Bootstrap from MaxCDN to your Genesis WordPress site.
 Author: Nate Finch
 Version: 1.0
 Author URI: http://www.finchproservices.com
@@ -10,9 +10,9 @@ Author URI: http://www.finchproservices.com
 
 function n8f_get_bootstrap() {
  
-    wp_enqueue_style('n8f-styles', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css', __FILE__ ) );
+    wp_enqueue_style('n8f-styles', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css');
     wp_enqueue_script( 'n8f-scripts', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js', array( 'jquery' ), '1.0' );
     
 }
 
-add_action( 'wp_enqueue_scripts', 'n8f_get_bootstrap' );
+add_action( 'genesis_meta', 'n8f_get_bootstrap' );
