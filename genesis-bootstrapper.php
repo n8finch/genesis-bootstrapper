ec1c6c6188783dfa85029032b760e52eef671014
Nate Finch <finchproservices@gmail.com>
2015-04-21 07:11:59 +0900

    first save

---
+++ genesis-bootstrapper.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Genesis Bootstrapper
+Plugin URI: http://www.finchproservices.com
+Description: This is a simple plugin that will enque Bootstrap to your Genesis WordPress site.
+Author: Nate Finch
+Version: 1.0
+Author URI: http://www.finchproservices.com
+*/
+
